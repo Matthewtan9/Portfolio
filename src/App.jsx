b6820711b6903dc76d987c1beb71ffccd2155b73
Matthewtan9 <matthew999099@gmail.com>
2024-12-19 10:30:14 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
+import { Footer } from "./components/Footer/Footer";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
+
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Experience />
       <Projects />
       <Contact />
+      <Footer/>
+
     </div>
   );
 }
